de7e197563962e05f5aff41f154d895a90133555
joseacuna <joseacunaacuna@gmail.com>
2014-04-06 16:12:02 -0300

    Se sube la tarea1 de ayudantia en ISW

---
+++ Index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Grupo 8</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<div id="contenido">
+    <header>
+        <div id="Titulo Principal">
+            <h1>Tarea N°2</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href='Index.php'>Jose Acuña</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+
+        <div id='textopr'>
+            <article>
+                <div class="Foto"><img  style="float:right" src="/Tarea1Ayudantia/Fotos/jose.jpg" alt ='jose' width='160' height='150'></div>
+                <div id='NombreTitulo'>
+                    <h1>José Acuña</h1>
+                </div>
+                <p>Nombre: Jose Acuña</p>
+                <p> Rut:16.381.843-4</p>
+                <p> Carrera:Ingenieria en Informatica.</p>
+                <p>  Asignatura: Ingenieria en Software</p>
+            </article>
+            <aside>
+                <p>
+                    Visita Nuestro Repositorio <a href="https://github.com/joseacuna"> Github</a>
+                </p>
+
+            </aside>
+        </div>
+    </section>
+</div>
+</body>
+
+<footer>
+    <p>
+    <?
+    $vector = array(
+    1=>"”Siempre ha de haber dos, ni más ni menos. Un maestro, y un aprendiz.” Maestro Yoda.",
+    2=>"”No lo intentes. Hazlo, o no lo hagas, pero no lo intentes.” Maestro Yoda",
+    3=>"”Si hablas con Dios eres religioso, si Dios habla contigo, eres psicópata.”  Gregory House",
+    4=>"”Gánate el respeto de los demás teniendo la osadía de ser tú mismo.”  Gregory House",
+    5=>"”El amigo ha de ser como el dinero, que antes de necesitarlo, se sabe el valor que tiene.” Socrátes",
+    6=>"” La ciencia humana consiste más en destruir errores que en descubrir verdades.” Socrátes",
+    );
+    $numero = rand(1,6);
+    echo "$vector[$numero]";
+?>
+    </p>
+</footer>
+</html>
